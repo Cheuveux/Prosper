4b7757f8a2736e3a362c7220531af7c7eadb870a
Cheveux <gaspard.szuszkin@gmail.com>
2024-06-10 19:25:31 +0200

    TripleSchnekerz

--- carousel.js
+++ carousel.js
@@ -1,3 +1,4 @@
+//DISPLAY PAR CLICK DES IMGS DU SLIDE DANS CAROUSEL-PORTRAIT/PAYSAGE
 
   document.querySelectorAll('.image .portrait').forEach(image=>{
     image.onclick = () =>{
@@ -14,25 +15,53 @@
         document.querySelector('.carousel-paysage img').src= image.getAttribute('src');
     }  
     });
-  
-  
 
+  //SCROLL PAR FLECHE DANS CAROUSEL-SLIDE
 
-
-
-  
-
-
-
-
-
-/* document.querySelectorAll('img.portrait').forEach(image=>{
-  image.onclick = () =>{
-      document.querySelector('.popup-portrait').style.display='block';
-      document.querySelector('.popup-portrait img').src= image.getAttribute('src');
-  }  
+    document.getElementById('backBtn').addEventListener('click', function() {
+      document.querySelector('.carousel-slide').scrollBy({
+          left: -150, // Ajustez cette valeur selon la distance souhaitée
+          behavior: 'smooth'
+      });
   });
 
-document.querySelector('.popup-portrait span').onclick =() => {
-  document.querySelector('.popup-portrait').style.display='none';
-} */
+  document.getElementById('nextBtn').addEventListener('click', function() {
+      document.querySelector('.carousel-slide').scrollBy({
+          left: 150, // Ajustez cette valeur selon la distance souhaitée
+          behavior: 'smooth'
+      });
+  });
+
+
+//DEF DE FONCTIONS DISPLAY PHOTOS PREV/NEXT PAR CLICK SUR FLECHES
+
+
+var carousel_img = document.querySelector('.carousel-img');
+var images = ['photos/3130x2075.JPG', 'photos/2075x3130-e.JPG', 'photos/3130x2075-a.JPG', 'photos/2048 x 3091.JPG', 'photos/2075 x 3130-c.JPG', 'photos/3130x2075-b.jpg', 'photos/2075x3130-b.JPG', 'photos/2075x3130-d.JPG', 'photos/3130x2075-c.jpg', 'photos/2075x3130-f.jpg', 'photos/2075x3130.JPG'];
+var i=0;
+
+function prev() {
+  if(i<=0) i=images.length;
+  i--;
+  return setImg ();
+}
+
+function next() {
+  if(i<=0) i=images.length;
+  i--;
+  return setImg ();
+}
+
+function setImg () {
+  return carousel_img.setAttribute ('src', 'photos/' +images [i]);
+}
+
+
+
+
+  
+
+
+
+
+
